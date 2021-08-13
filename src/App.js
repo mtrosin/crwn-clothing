d7c392bc5e2aa6e905a15028341e71ec162f2a74
mtrosin <mtrosin@gmail.com>
2021-08-12 23:50:58 -0300

    adding sign in form and buttons components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-)
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
@@ -21,7 +16,7 @@ function App() {
             <Switch>
                 <Route exact path='/' component={HomePage} />
                 <Route path='/shop' component={ShopPage} />
-                <Route path='/shop/hats' component={HatsPage} />
+                <Route path='/signin' component={SignInAndSignUpPage} />
             </Switch>
         </BrowserRouter>
     </div>
