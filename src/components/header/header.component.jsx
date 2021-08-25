2fe1fc1d259993d2af695ea2164872fd039bd59c
mtrosin <mtrosin@gmail.com>
2021-08-24 23:40:32 -0300

    added redux, and implemented userReducer and userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -10,25 +11,29 @@ import './header.styles.scss';
 const Header = ({ currentUser }) => (
     <div className='header'>
         <BrowserRouter>
-        <Link className='logo-container' to="/">
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
+            <Link className='logo-container' to="/">
+                <Logo className='logo' />
             </Link>
-            <Link className='option' to='/shop'>
-                CONTACT
-            </Link>
-            {
-                currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
-            }
-        </div>
+            <div className='options'>
+                <Link className='option' to='/shop'>
+                    SHOP
+                </Link>
+                <Link className='option' to='/shop'>
+                    CONTACT
+                </Link>
+                {
+                    currentUser ?
+                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className='option' to='/signin'>SIGN IN</Link>
+                }
+            </div>
         </BrowserRouter>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
